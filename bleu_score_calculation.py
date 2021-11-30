6190540537242a647ace32d68440187fe66f6b8c
Piyush Mital <piyushmital@Piyushs-MacBook-Air.local>
2021-11-30 11:23:27 -0500

    Calculation of BLEU scores of all output files till now

--- bleu_score_calculation.py
+++ bleu_score_calculation.py
@@ -1,35 +1,58 @@
 import nltk
 import sys
+import os
+from datetime import datetime
 from nltk.translate.bleu_score import SmoothingFunction, corpus_bleu
 
 print(" Arguments are : ", sys.argv[1], sys.argv[2])
 # There can be multiple references but 1 hypothesis
-reference_file = open(sys.argv[1], 'r')
-reference_lines = reference_file.readlines()
 
-hypothesis_file = open(sys.argv[2], 'r')
-hypothesis_lines = hypothesis_file.readlines()
-count = 0
-references = []
-hypothesis = []
-pair_reference = []
-for line in reference_lines:
-    if count%2 != 0:
-        references.append(pair_reference)
-        pair_reference = []
+# current date and time
+now = datetime.now()
 
-    pair_reference.append(line.split())
-    count += 1
-    # references.append([line.split()])
+for folder in os.listdir("data/outputs"):
+    try:
+         if "output" not in folder:
+            continue
+         f = open("data/bleu-outputs/%s" %(now), "a")
+         for filename in os.listdir("data/outputs/%s" %(folder)):
+            try:
+                if "txt" not in filename:
+                    continue
+                reference_file = open("data/outputs/%s/%s" %(folder, filename), 'r')
+                reference_lines = reference_file.readlines()
 
-print(references)
-for line in hypothesis_lines:
-    hypothesis.append(line.split())
+                hypothesis_file = open("data/test.response", 'r')
+                hypothesis_lines = hypothesis_file.readlines()
+                count = 0
+                references = []
+                hypothesis = []
+                pair_reference = []
+                for line in reference_lines:
+                    if len(hypothesis_lines) != len(reference_lines):
+                        if count%2 != 0:
+                            references.append(pair_reference)
+                            pair_reference = []
 
-print(hypothesis)
+                        pair_reference.append(line.split())
+                        count += 1
+                    else:
+                        references.append([line.split()])
 
-cc = SmoothingFunction()
+                # print(references)
+                for line in hypothesis_lines:
+                    hypothesis.append(line.split())
 
-bleu_score = nltk.translate.bleu_score.corpus_bleu(references, hypothesis,weights = [1], smoothing_function=cc.method3)
-print(bleu_score)
+                # print(hypothesis)
+
+                cc = SmoothingFunction()
+
+                bleu_score = nltk.translate.bleu_score.corpus_bleu(references, hypothesis,weights = [1], smoothing_function=cc.method3)
+                f.write("%s :: %s :: %s \n" %(folder,filename, bleu_score))
+
+            except:
+                print(" Failed for given filename %s" %(filename))
+         f.write("\n")
+    except:
+        print("Failed for give folder %s" %(folder))
 
